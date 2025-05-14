d1429e30fe1c4fccf11c417ff49ae6d1251db571
TofunmiTranspose <tofexzyno@gmail.com>
2025-05-14 04:33:01 -0700

    Add files via upload

---
+++ Day1/app.js
@@ -0,0 +1,17 @@
+window.addEventListener('click', ()=>{
+    document.querySelector('input').focus();
+  });
+  window.addEventListener('keydown', p);
+function p(e) {
+  d = (d) => document.querySelector(d);
+  k = e.keyCode;
+  a = d(`audio[data-key="${k}"]`);
+  key = d(`div[data-key="${k}"]`);
+  if (!a) return;
+  key.classList.add('playing');
+  a.currentTime = 0;
+  a.play();
+  setTimeout(() => {
+    key.classList.remove('playing');}
+  ,50); 
+};
